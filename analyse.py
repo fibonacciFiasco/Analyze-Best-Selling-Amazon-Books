ba9140a9ba089ce3e96c2ea05a664287b2e0cedb
Diya Patel <pateldiya1005@gmail.com>
2026-05-22 15:10:59 +0530

    Enhance data analysis with cleaning and exports
    Added data cleaning, renaming columns, and export functionality for top authors and average ratings.

--- analyse.py
+++ analyse.py
@@ -4,3 +4,17 @@ print(df.head())
 print(df.shape)
 print(df.columns)
 print(df.describe())
+
+
+df.drop_duplicates(inplace=True)
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+df["Price"] = df["Price"].astype(float) # The code convers "Price column to a float data type"
+author_counts = df['Author'].value_counts()
+print(author_counts)
+avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
+print(avg_rating_by_genre)
+# Export top selling authors to a CSV file
+author_counts.head(10).to_csv("top_authors.csv")
+
+# Export average rating by genre to a CSV file
+avg_rating_by_genre.to_csv("avg_rating_by_genre.csv")
